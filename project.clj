e3149bcafe5c570244bff58dcc8af5d771e39fbe
Paul Landes <landes@mailc.net>
2016-11-29 11:03:15 -0600

    doc

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Apache License version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :plugins [[lein-codox "0.9.5"]
+  :plugins [[lein-codox "0.10.1"]
             [org.clojars.cvillecsteele/lein-git-version "1.0.3"]]
   :codox {:metadata {:doc/format :markdown}
           :project {:name "Generate, split into folds or train/test and cache a dataset"}
-          :output-path "target/doc/codox"}
+          :output-path "target/doc/codox"
+          :source-uri "https://github.com/plandes/clj-ml-dataset/blob/v{version}/{filepath}#L{line}"}
   :source-paths ["src/clojure"]
   :javac-options ["-Xlint:unchecked"]
   :exclusions [org.slf4j/slf4j-log4j12
